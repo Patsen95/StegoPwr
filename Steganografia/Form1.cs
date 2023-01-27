c6fbf9037f7c44a0fbcfadabf48077eb15345be0
Patsen95 <patsen995@gmail.com>
2023-01-27 01:51:53 +0100

    Final commit

--- Steganografia/Form1.cs
+++ Steganografia/Form1.cs
@@ -2,37 +2,62 @@ namespace Steganografia
 {
     public partial class Form1 : Form
     {
-        private Bitmap bmp;
-        private string msg;
+        private Bitmap bmp = null;
+        private string theText = string.Empty;
+        private int maxChars = 0;        
 
         public Form1()
         {
             InitializeComponent();
 
+            #region Ustawienia kontrolek
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            StartPosition = FormStartPosition.CenterScreen;
             MaximizeBox = false;
+            Text = "Steganosaur";
+            
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            btn_Export.Enabled = false;
+            btn_Encode.Enabled = false;
+            btn_Decode.Enabled = false;
 
-            #region Podpisy kontrolek
-            tabPage1.Text = "Kodowanie";
-            tabPage2.Text = "Dekodowanie";
+            FormClosed += Form1_FormClosed;
+            btn_LoadImg.Click += Btn_LoadImg_Click;
+            btn_Clear.Click += Btn_Clear_Click;
+            btn_Export.Click += Btn_Export_Click;
+            btn_Encode.Click += Btn_Encode_Click;
+            btn_Decode.Click += Btn_Decode_Click;
+            btn_Author.Click += Btn_Author_Click;
+            textBox1.TextChanged += TextBox1_TextChanged;
+
             groupBox1.Text = "Podgl¹d obrazu";
             groupBox2.Text = "Informacje";
-            groupBox3.Text = "Ustawienia";
+            groupBox3.Text = "Wiadomoœæ";
 
-            label1.Text = "Nazwa pliku:";
-            label2.Text = "Rozmiar[px]:";
-            label3.Text = "Rozmiar na dysku[kB]:";
-            label3.Text = "Rozmiar na dysku[kB]:";
+            label3.Text = string.Empty;
+            label4.Text = string.Empty;
+            label6.Text = string.Empty;
+            label8.Text = "0";
 
             btn_LoadImg.Text = "Wczytaj obraz...";
             btn_Export.Text = "Eksportuj";
+            btn_Clear.Text = "Wyczyœæ";
+            btn_Encode.Text = "Zakoduj";
+            btn_Decode.Text = "Odkoduj";
+            btn_Author.Text = "?";
             #endregion
-            
-
         }
-        
+
+        // Sprz¹tanie zasobów po zamkniêciu g³ównej formatki
+        private void Form1_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            if(bmp != null)
+                bmp.Dispose();
+        }
+
         // Wczytywanie obrazka
-        private void btn_LoadImg_Click(object sender, EventArgs e)
+        private void Btn_LoadImg_Click(object? sender, EventArgs e)
         {
             using(OpenFileDialog ofd = new OpenFileDialog())
             {
@@ -42,9 +67,91 @@ namespace Steganografia
 
                 if(ofd.ShowDialog() == DialogResult.OK)
                 {
+                    bmp = new Bitmap(ofd.FileName);
+                    maxChars = GetMaxCodingLength(bmp);
 
+                    pictureBox1.Image = bmp;
+                    label3.Text = ofd.SafeFileName;
+                    label4.Text = string.Format("{0} x {1}", bmp.Size.Width, bmp.Size.Height);
+                    label6.Text = string.Format("{0:N0} ({1} kB)", maxChars, (maxChars / 1000));
+                    btn_Decode.Enabled = true;
                 }
             }
+        }
+
+        // Przycisk eksportu
+        private void Btn_Export_Click(object? sender, EventArgs e)
+        {
+            using(SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Title = "Zapisz obraz";
+                sfd.Filter = "PNG (*.png)|*.png|BMP (*.bmp)|*.bmp";
+                sfd.RestoreDirectory = true;
+
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    bmp.Save(sfd.FileName);
+                    MessageBox.Show("Obraz pomyœlnie wyeksportowano!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        // Zakodowanie danych
+        private void Btn_Encode_Click(object? sender, EventArgs e)
+        {
+            if(textBox1.TextLength >= maxChars)
+            {
+                MessageBox.Show(string.Format("D³ugoœæ tekstu przekracza maksymalny dostêpny zakres: {0}.", maxChars), "B³¹d kodowania", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            theText = textBox1.Text;
+            bmp = Steganosaur.Encode(bmp, theText);
+        }
+
+        // Odkodowanie danych
+        private void Btn_Decode_Click(object? sender, EventArgs e)
+        {
+            textBox1.Text = Steganosaur.Decode(bmp);
+        }
+
+        // Czyszczenie okienka tekstowego
+        private void Btn_Clear_Click(object? sender, EventArgs e)
+        {
+            if(textBox1.TextLength > 0)
+                textBox1.Clear();
+        }
+
+        // Informacja o autorze
+        private void Btn_Author_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("\"Steganosaur\" - aplikacja pozwalaj¹ca ukryæ dowolny tekst w obrazku." +
+                            "\nPatryk Sienkiewicz (221158)\nPolitechnika Wroc³awska, WEFiM 2023",
+                            "Autor", MessageBoxButtons.OK, MessageBoxIcon.Information
+                        );
+        }
+
+        // Zdarzenie podczas zmiany tekstu
+        private void TextBox1_TextChanged(object? sender, EventArgs e)
+        {
+            label8.Text = textBox1.TextLength.ToString("N0");
+            if((textBox1.TextLength > 0) && (bmp != null))
+            {
+                btn_Encode.Enabled = true;
+                btn_Export.Enabled = true;
+            }
+            else
+            {
+                btn_Encode.Enabled = false;
+                btn_Export.Enabled = false;
+            }
+        }
+
+        // Dodatkowa metoda pozwalaj¹ca obliczyæ przybli¿on¹ liczbê bajtów (znaków),
+        // które da siê zmieœciæ we wczytanym obrazku
+        private int GetMaxCodingLength(Bitmap bmp)
+        {
+            int _pixels = bmp.Width * bmp.Height;
+            return (int)(_pixels / 3);
         }
     }
 }
--- Steganografia/Steganosaur.cs
+++ Steganografia/Steganosaur.cs
@@ -1,28 +1,162 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Steganografia
+﻿namespace Steganografia
 {
     public static class Steganosaur
     {
-        public static Bitmap Encode(string text)
+        // Kodowanie tekstu
+        public static Bitmap Encode(Bitmap bmp, string text)
         {
+            // Wartości subpikseli - składowych kolorów
+            int R = 0;
+            int G = 0;
+            int B = 0;
 
-            return null;
+            int chVal = 0; // Wartość pojedynczego znaku
+            int chIdx = 0; // Indeks ukrywanego znaku ze stringa 'text'
+            int zeros = 0; // Zlicza zera - potrzebne jako wskaźnik zakończenia kodowania
+            int bytePos = 0; // Śledzi aktualną ilość zakodowanych bitów
+
+            bool ending = false; // Flaga pomocnicza zakończenia bajtu
+
+            // Przelatujemy przez wysokość i szerokość obrazka
+            for(int y = 0; y < bmp.Height; y++)
+            {
+                for(int x = 0; x < bmp.Width; x++)
+                {
+                    // Zbieramy kolor piksela. Następnie zerujemy najmniej znaczący bit (LSB) każdej jego składowej.
+                    Color pxl = bmp.GetPixel(x, y);
+                    R = pxl.R - pxl.R % 2;
+                    G = pxl.G - pxl.G % 2;
+                    B = pxl.B - pxl.B % 2;
+
+                    // Iterujemy przez 3 składowe (R, G, B)...
+                    for(int i = 0; i < 3; i++)
+                    {
+                        if(bytePos % 8 == 0) // Sprawdzamy, czy cały bajt został zakodowany
+                        {
+                            // Sprawdzamy zakończenie kodowania - jeśli po drodze
+                            // zliczonych zostało kolejno 8 zer w kolejnych składowych i zakodowany
+                            // został cały bajt...
+                            if(zeros == 8 && ending == true)
+                            {
+                                // ...to sprawiamy by zakodowany został nawet niepełny bajt składowej.
+                                // 3 bajty piksela RGB dają łącznie 24 bity, a pojedynczy znak waży 1 bajt (8 bitów).
+                                // Oznacza to, że potrzeba ponad 2.5 piksela (a dokładnie 8 z 9 bajtów) na ukrycie w nim zaledwie
+                                // jednego znaku - w każdym subpikselu kodowany jest JEDEN BIT znaku.
+                                if((bytePos - 1) % 3 < 2)
+                                    bmp.SetPixel(x, y, Color.FromArgb(R, G, B));
+                                return bmp; // Na koniec zwracamy obrazek ze zmodyfikowaną zawartością i ukrytym tekstem
+                            }
+
+                            // Sprawdzamy czy licznik znaków jest przynajmniej równy długości tekstu
+                            // i ustawiamy flagę zakończenia kodowania.
+                            // Jeśli nie, to pobieramy kolejny znak.
+                            if(chIdx >= text.Length)
+                                ending = true;
+                            else
+                                chVal = text[chIdx++];
+                        }
+
+                        // Tu się odbywa właściwe kodowanie...
+                        switch(bytePos % 3)
+                        {
+                            case 0:
+                                if(!ending)
+                                {
+                                    // W każdym subpikselu zwyczajnie dodajemy 1 do LSB,
+                                    // a potem usuwany ten zakodowany bit w znaku.
+                                    R += chVal % 2;
+                                    chVal /= 2;
+                                }
+                                break;
+
+                            case 1:
+                                if(!ending)
+                                {
+                                    G += chVal % 2;
+                                    chVal /= 2;
+                                }
+                                break;
+
+                            case 2:
+                                if(!ending)
+                                {
+                                    B += chVal % 2;
+                                    chVal /= 2;
+                                }
+                                bmp.SetPixel(x, y, Color.FromArgb(R, G, B)); // Nadpisujemy piksel.
+                                break;
+                        }
+
+                        // Inkrementujemy liczniki
+                        bytePos++;
+                        if(ending)
+                            zeros++;
+                    }
+                }
+            }
+            return bmp;
         }
 
+        // Odkodowanie
         public static string Decode(Bitmap bmp)
         {
+            string str = string.Empty; // Zmienna przechowująca rozkodowany tekst
+            int chVal = 0;  // Wartość pojedynczego znaku
+            int bytePos = 0; // Licznik pozycji w bajcie
 
-            return string.Empty;
+            // Lecimy przez wysokość i szerokość obrazka...
+            for(int y = 0; y < bmp.Height; y++)
+            {
+                for(int x = 0; x < bmp.Width; x++)
+                {
+                    Color col = bmp.GetPixel(x, y); // Odczytujemy kolor
+                    
+                    // Iterujemy po składowych...
+                    for(int subPx = 0; subPx < 3; subPx++)
+                    {
+                        switch(bytePos % 3)
+                        {
+                            case 0:
+                                // Bierzemy LSB z subpiksela i dodajemy do kolejnych
+                                // bitów w znaku. Warto zauważych, że robi się to od prawej do lewej,
+                                // Więc na koniec cały bajt trzeba przerzucić - zamienić bity miejscami - MSB na LSB.
+                                chVal = chVal * 2 + col.R % 2;
+                                break;
+
+                            case 1:
+                                chVal = chVal * 2 + col.G % 2;
+                                break;
+
+                            case 2:
+                                chVal = chVal * 2 + col.B % 2;
+                                break;
+                        }
+                        bytePos++;
+
+                        // Gdy cały bajt zostanie rozkodowany...
+                        if(bytePos % 8 == 0)
+                        {
+                            chVal = ReverseBits(chVal); // ...zamieniamy miejscami bity...
+                            if(chVal == 0)
+                                return str;
+                            str += (char)chVal; // ...i składamy do wyjściowego stringa
+                        }
+                    }
+                }
+            }
+            return str; // Zwracamy odkodowany tekst
         }
 
+        // Metoda pomocnicza do odwracania kolejności bitów
         private static int ReverseBits(int n)
         {
-            return 0;
+            int res = 0;
+            for(int i = 0; i < 8; i++)
+            {
+                res = res * 2 + n % 2;
+                n /= 2;
+            }
+            return res;
         }
     }
 }
